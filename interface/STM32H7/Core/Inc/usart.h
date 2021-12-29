729faf8f90e794ed5df94689683b99eeea2b2eef
SilverSoul <1079931505@qq.com>
2021-12-29 17:12:30 +0800

    update

--- interface/STM32H7/Core/Inc/usart.h
+++ interface/STM32H7/Core/Inc/usart.h
@@ -44,10 +44,7 @@ void MX_USART2_UART_Init(void);
 void MX_USART3_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
-void Motor_Rotate(int PID, int PWM, int TIME);
-void Num2str(int num, int digit);
-char digit2str(int num);
-char *myitoa(int value, char *string, int radix);
+
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
